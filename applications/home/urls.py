db8db5d597e3764928b8b82ab2d517659ed32c9b
Ontiveros Alfaro Johan Alejandro <johalfaro1174@gmail.com>
2024-09-09 19:22:46 -0600

    View updated and first model created.

--- applications/home/urls.py
+++ applications/home/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, PruebaListView
 
 urlpatterns = [
-    path('home/', IndexView.as_view())
+    path('home/', IndexView.as_view()),
+    path('home/lista/', PruebaListView.as_view())
 ]
---
+++ django_course_employes/settings/prod.py
@@ -0,0 +1,25 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+ALLOWED_HOSTS = []
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-course-employes',
+        'USER': 'postgres',
+        'PASSWORD': 'Soyjohan123.',
+        'HOST': 'localhost',
+        'PORT': '5432'
+    }
+}
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/5.1/howto/static-files/
+
+STATIC_URL = 'static/'
